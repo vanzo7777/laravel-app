a816872b3fb3cd9378ba6c07802cab37fde22c14
Praktikant <Praktikant@kubus.local>
2026-04-08 13:54:32 +0200

    Setup Article + ArticleFaq models

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'excerpt',
+        'content',
+        'publish_date', 
+        'author',
+        'image',
+    ];
+
+    public function faqs() {
+        return $this->hasmany(ArticleFaq::class)->orderBy('sort_order');
+    }
 }
--- app/Models/ArticleFaq.php
+++ app/Models/ArticleFaq.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class ArticleFaq extends Model
 {
-    //
+    protected $fillable = [
+        'article_id', 
+        'question', 
+        'asnwer', 
+        'sort_order',
+    ];
+
+    public function article() {
+        return $this->belongsTo(Article::class);
+    }
 }
